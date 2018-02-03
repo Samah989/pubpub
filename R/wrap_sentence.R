03dc6774562b9e1f73bcd8a67dc87cf7a31fae56
Richard Blissett <rsl.bliss@gmail.com>
2018-02-03 13:52:54 -0500

    First commit

---
+++ R/wrap_sentence.R
@@ -0,0 +1,19 @@
+#' A string wrapping function
+#' 
+#' This function takes a space-delimited sentence and inserts newlines to wrap the sentence. 
+wrap_sentence <- function(string, width=30) {
+  words <- unlist(strsplit(string, " "))
+  fullsentence <- ""
+  checklen <- ""
+  for(i in 1:length(words)) {
+    checklen <- paste(checklen, words[i])
+    if(nchar(checklen)>(width+1)) {
+      fullsentence <- paste0(fullsentence, "\n")
+      checklen <- ""
+    }
+    fullsentence <- paste(fullsentence, words[i])
+  }
+  fullsentence <- sub("^\\s", "", fullsentence)
+  fullsentence <- gsub("\n ", "\n", fullsentence)
+  return(fullsentence)
+}
