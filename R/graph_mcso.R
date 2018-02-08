d75b9535026dba70ce926582a58d2064278b6f39
Richard Blissett <rsl.bliss@gmail.com>
2018-02-08 14:36:35 -0500

    Added custom option to graph_mcso

--- R/graph_mcso.R
+++ R/graph_mcso.R
@@ -2,20 +2,25 @@
 #' 
 #' This function creates a horizontal bar graph for a multiple choice, single option question
 #' @export
-graph_mcso <- function(item, sort="alpha") {
+graph_mcso <- function(item, sort="alpha", custom=NULL) {
   sorted <- data.frame(table(item))
   sort="alpha"
   if(sort=="descending") {
     sorted <- sorted[order(sorted$Freq),]
+    sorder <- sorted$item
   } else if(sort=="ascending") {
     sorted <- sorted[order(-sorted$Freq),]
+    sorder <- sorted$item
   } else if(sort=="alpha") {
     sorted <- sorted[order(sorted$item),]
     sorted <- sorted[rev(1:nrow(sorted)),]
+    sorder <- sorted$item
+  } else if(sort=="custom") {
+    sorder <- custom
   } else {
     stop("sort parameter must equal \"alpha\", \"ascending\", or \"descending\"")
   }
-  sorted$item <- factor(sorted$item, ordered=T, levels=sorted$item)
+  sorted$item <- factor(sorted$item, ordered=T, levels=sorder)
   return(ggplot(sorted, aes(x=item, y=Freq)) +
            geom_bar(stat="identity") +
            xlab("") +
