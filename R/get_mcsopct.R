3bcaba9de6ec4a70435fb4f24ca07eed32507b1f
Richard Blissett <rsl.bliss@gmail.com>
2018-02-08 15:20:23 -0500

    Forgot to multiple by 100

--- R/get_mcsopct.R
+++ R/get_mcsopct.R
@@ -3,5 +3,5 @@
 #' Gets percentage of values in a multiple choice question with specific value
 #' @export
 get_mcsopct <- function(item, value, digits=2) {
-  return(as.numeric(round(digits=digits, prop.table(table(item))[value])))
+  return(as.numeric(round(digits=digits, 100*prop.table(table(item))[value])))
 }
