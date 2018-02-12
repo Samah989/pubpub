5a79173008d440443797c08173d3f7280b11ac81
Richard Blissett <rsl.bliss@gmail.com>
2018-02-12 12:52:14 -0500

    Added graph_numeric

---
+++ R/graph_numeric.R
@@ -0,0 +1,16 @@
+#' Graphs a numeric question
+#' 
+#' This function creates a histogram for a numeric question
+#' @export
+graph_numeric <- function(item, unit, xmin=0, xmax=max(item)+binwidth, binwidth=1) {
+  data <- data.frame(item)
+  names(data) <- "Item"
+  return(ggplot(data) +
+           geom_histogram(aes(x=Item), binwidth=1) +
+           xlab(unit) +
+           ylab("Count") +
+           scale_x_continuous(expand=c(0, 0), limits=c(xmin, xmax)) +
+           scale_y_continuous(expand=c(0, 0)) +
+           theme(panel.background=element_blank(),
+                 axis.line=element_line(colour="black")))
+}
