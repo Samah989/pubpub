93ce3ac9b1a810dda44c8c6e67f9deec82c4d6cb
Richard Blissett <rsl.bliss@gmail.com>
2018-02-14 14:51:46 -0500

    x scale is messed up, fixed it

--- R/graph_numeric.R
+++ R/graph_numeric.R
@@ -5,8 +5,9 @@
 graph_numeric <- function(item, unit, xmin=0, xmax=max(item)+binwidth, binwidth=1, fill="darkmagenta") {
   data <- data.frame(item)
   names(data) <- "Item"
+  xmin <- xmin-1
   return(ggplot(data) +
-           geom_histogram(aes(x=Item), binwidth=1, fill=fill) +
+           geom_histogram(aes(x=Item), binwidth=binwidth, fill=fill) +
            xlab(unit) +
            ylab("Count") +
            scale_x_continuous(limits=c(xmin, xmax)) +
