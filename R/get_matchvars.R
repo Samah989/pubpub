f0ff7187dd17c40dea86c3749352a4babb03379c
Richard Blissett <rsl.bliss@gmail.com>
2018-03-09 20:09:41 -0500

    Added get_matchvars() documentation

--- R/get_matchvars.R
+++ R/get_matchvars.R
@@ -1,6 +1,17 @@
-#' Gets list of variables with a certain substring in them
+#' Returns list of variables matching matters
 #' 
-#' Searches through all variable names and returns all that match a search term
+#' Searches through all variable names and returns all that match a search
+#' term, using regular expressions.
+#' 
+#' @param d The full data frame
+#' @param pattern The pattern to match in the variable name
+#' 
+#' @examples
+#' # Get all variable names including "Q6_"
+#' get_matchvars(survey, "Q6_")
+#' # Data frame with only Q6 variables
+#' survey[get_matchvars(survey, "Q6_")]
+#' 
 #' @export
 get_matchvars <- function(d, pattern) {
   return(names(d)[grepl(pattern, names(d))])
